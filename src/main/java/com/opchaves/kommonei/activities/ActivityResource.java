17ad103df7be4a3582a23029a172e7910027793f
opchaves <opaulochaves@gmail.com>
2024-09-19 13:46:33 -0300

    disable devservices for mongodb in favor of docker-compose.yml

--- src/main/java/com/opchaves/kommonei/activities/ActivityResource.java
+++ src/main/java/com/opchaves/kommonei/activities/ActivityResource.java
@@ -12,7 +12,6 @@ import org.eclipse.microprofile.openapi.annotations.Operation;
 import org.eclipse.microprofile.openapi.annotations.media.Content;
 import org.eclipse.microprofile.openapi.annotations.media.ExampleObject;
 import org.eclipse.microprofile.openapi.annotations.media.Schema;
-import org.eclipse.microprofile.openapi.annotations.responses.APIResponses;
 import org.eclipse.microprofile.openapi.annotations.responses.APIResponse;
 import org.eclipse.microprofile.openapi.annotations.tags.Tag;
 
